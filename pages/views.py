3e2c92e67bf8c2ca2213b280333caa4d80b8a7dd
Jeff-GT <jefferson.guerrero@sdgku.edu>
2025-01-29 18:14:15 -0800

    Final Commit

---
+++ pages/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from .forms import ContactForm
+from django.core.mail import send_mail
+
+def home_view(request):
+    return render(request, 'pages/home.html')
+
+def about_view(request):
+    return render(request, 'pages/about.html')
+
+def contact_view(request):
+    if request.method == "POST":
+        print("sending email")
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            print("form is valid")
+
+            message = form.cleaned_data['message']
+   
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+
+            message_body = f"This is an email from your page. \nName: {name}\nEmail: {email}\nMessage: {message}"
+
+            send_mail(
+                "Portfolio Email",
+                message_body,
+                email,
+                ["jeffturla0615@gmail.com"],
+            )
+
+            
+            return render(request, 'pages/contact.html', {'form': form, "success": True})
+
+        else: 
+            print("Invalid")
+    else:
+        form = ContactForm()
+
+    return render(request, 'pages/contact.html', {'form': form})
+
+
+
+def projects_view(request):
+    return render(request, 'pages/projects_list.html')
